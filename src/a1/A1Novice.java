92118349dfd89fb7e4d51774c818832ed5165b0a
jomcsp <jomcsp@jomcsp-pc.wireless-1x.unc.edu>
2019-08-27 17:48:20 -0400

    wrong file fix

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,76 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+	
 		
+				int count = scan.nextInt();
+				
+				
+				int[] custNum = new int [count];
+				String[] custFName = new String [count];
+				String[] custLName = new String [count];
+				int[] totalNum = new int [count];
+				int[] itemNum = new int [totalNum.length];
+				String [] itemName = new String [totalNum.length];
+				double[] itemPrice = new double [totalNum.length];
+				double[] totalPrice = new double [count];
+				
+				for (int i = 0; i < custNum.length; i++) {
+					custFName[i] = scan.next();
+					custLName[i] = scan.next();
+					totalNum[i] = scan.nextInt();
+					for (int z = 0; z < totalNum[i]; z++) {
+						itemNum[z] = scan.nextInt();
+						itemName[z] = scan.next();
+						itemPrice[z] = scan.nextDouble();
+					}
+					totalPrice[i] = sum(itemNum, itemPrice);
+					for (int j = 0; j < totalNum[i]; j++) {
+						itemNum[j] = 0;
+						itemPrice[j] = 0;
+					}
+					
+					
+					
+				}
+				//end of scan
+				
+				scan.close();
+				
+				for (int i = 0; i < count; i++) {
+					System.out.println(firstNameConverter(custFName, i) + " " + custLName[i] + ": " + totalPrice[i]) ;
+				}
+				
+				
+				
+				
+				
+				
+				
+				
+			}
+			
+			// new method First name converter
+			
+		static String firstNameConverter(String[] custFName, int num) {
+			
+			char x = custFName[num].charAt(0);
+			
+			
+			return x + ".";
+			}
+		  
+
+		// sum method
+		static double sum(int[] itemNum, double[] itemPrice) {
+			double x = 0.0;
+			for ( int i = 0; i < itemNum.length; i++) {
+				x += (itemNum[i] * itemPrice[i]);
+			}
+			
+			
+			return x;
+			
+		}
 	}
-}
+
